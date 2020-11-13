ee31794d7dd0eb22d7588f4a207d345c5f1a3e70
Rich King <rking@rebgv.org>
2020-11-13 14:43:58 -0800

    Added PrimeVue styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/global.css'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// The following setting enables animations for PrimeVue.
+app.config.globalProperties.$primevue = { ripple: true };
+
+app.mount('#app');
+

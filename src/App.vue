696553662225eb40af996902aa0c938698d59028
Rich King <rking@rebgv.org>
2020-11-13 12:43:20 -0800

    Moved some global CSS out of App.vue

--- src/App.vue
+++ src/App.vue
@@ -118,17 +118,3 @@ export default {
 
 </script>
 
-
-
-<style>
-
-#app {
-
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #444;
-}
-
-</style>
-

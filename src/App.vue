5f25777b537886174523d114b13da15ea078e2a5
Rich King <rking@rebgv.org>
2020-11-12 17:40:52 -0800

    Added partial data

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,116 @@
 
 </template>
 
+
+
 <script>
 
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+        groupings: [
+            {
+                name: "Multiple Listing Contract date",
+                fields: [
+                    { name: "contractDateDay", type: "days" },
+                    { name: "contractDateMonth", type: "months" },
+                    { name: "contractDateYear", type: "number" }
+                ]
+            },
+            {
+                name: "Property address",
+                fields: [
+                    { name: "addressUnitNumber", type: "string", friendlyName: "Unit" },
+                    { name: "addressHouseNumber", type: "string", friendlyName: "House number" },
+                    { name: "addressStreetName", type: "string", friendlyName: "Street name" },
+                    { name: "addressStreetType", type: "string", friendlyName: "Street type" },
+                    { name: "addressStreetDirection", type: "string", friendlyName: "Street direction" },
+                    { name: "addressCityOrTown", type: "string", friendlyName: "City or town" }
+                ]
+            },
+            {
+                name: "Location",
+                fields: [
+                    { name: "area", type: "string" },
+                    { name: "subarea", type: "string" }
+                ]
+            },
+            {
+                name: "Agent details",
+                fields: [
+                    { name: "designatedAgent1", type: "string" },
+                    { name: "designatedAgent2", type: "string" },
+                    { name: "designatedAgent3", type: "string" },
+                    { name: "userId1", type: "string" },
+                    { name: "userId2", type: "string" },
+                    { name: "userId3", type: "string" }
+                ]
+            },
+            {
+                name: "Showing information",
+                fields: [
+                    { name: "showingsNoRequirements", type: "checkbox" },
+                    { name: "showingsPhoneListingRealtorFirst", type: "checkbox" },
+                    { name: "showingsPhoneSellerFirst", type: "checkbox" },
+                    { name: "showingsPhoneTenantFirst", type: "checkbox" },
+                    { name: "showingsPhoneOther", type: "checkbox" },
+                    { name: "showingsTouchbase", type: "checkbox" },
+                    { name: "showingsShowingTime", type: "checkbox" }
+                ]
+            },
+            {
+                name: "Appointment contact",
+                fields: [
+                    { name: "appointmentPhoneNumber", type: "phone", friendlyName: "Phone number" },
+                    { name: "forAppointmentCall", type: "string", friendlyName: "For appointment call" }
+                ]
+            },
+            {
+                name: "Dwelling classification",
+                fields: [
+                    { name: "dwellingClassificationDetached", type: "radio" },
+                    { name: "dwellingClassificationAttached", type: "radio" },
+                    { name: "dwellingClassificationMultifamily", type: "radio" }
+                ]
+            },
+            {
+                name: "Type of dwelling",
+                fields: [
+                    { name: "detachedHouseSingleFamily", type: "radio" },
+                    { name: "detachedHouseWithAcreage", type: "radio" },
+                    { name: "detachedTownhouse", type: "radio" },
+                    { name: "detachedManufactured", type: "radio" },
+                    { name: "detachedManufacturedWithLand", type: "radio" },
+                    { name: "detachedRecreational", type: "radio" },
+                    { name: "detachedOther", type: "radio" },
+                    { name: "attachedApartmentCondo", type: "radio" },
+                    { name: "attachedHalfDuplex", type: "radio" },
+                    { name: "attachedRecreational", type: "radio" },
+                    { name: "attachedTownhouse", type: "radio" },
+                    { name: "attachedRowHouseNonStrata", type: "radio" },
+                    { name: "attachedOther", type: "radio" },
+                    { name: "multifamilyDuplex", type: "radio" },
+                    { name: "multifamilyTriplex", type: "radio" },
+                    { name: "multifamilyFourplex", type: "radio" },
+                    { name: "multifamilyRecreational", type: "radio" },
+                    { name: "multifamilyOther", type: "radio" }
+                ]
+            }
+        ]
+    }
   }
 }
 
 </script>
+
+
 
 <style>
 

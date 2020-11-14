e25cbfbf75f7cd505249144a443a89618aea4e10
Rich King <rking@rebgv.org>
2020-11-13 18:41:59 -0800

    Added PrimeVue checkbox support

--- src/components/FormCheckbox.vue
+++ src/components/FormCheckbox.vue
@@ -1,6 +1,11 @@
 <template>
   
-  <input type="checkbox">
+  <div class="container">
+  
+    <PrimeCheckbox v-model="testing" :value="friendlyName" :name="name" />
+    <label :for="name">{{ friendlyName }}</label>
+
+  </div>
 
 </template>
 
@@ -8,9 +13,23 @@
 
 <script>
 
+import Checkbox from 'primevue/checkbox';
+
 export default {
   
-  name: 'FormCheckbox'
+  name: 'FormCheckbox',
+  components: {
+    'PrimeCheckbox': Checkbox
+  },
+  data () {
+    return {
+      'testing': [] 
+    }
+  },
+  props: [
+    'friendlyName',
+    'name'
+  ]
 }
 
 </script>
@@ -19,6 +38,32 @@ export default {
 
 <style scoped>
 
+.container {
 
+  display: inline-block;
+  margin-bottom: 20px;
+}
+
+.p-checkbox {
+
+  margin-right: 10px;
+}
+
+label {
+
+  margin-right: 20px;
+}
+/* 
+.p-checkbox + span {
+
+  margin-bottom: 10px;
+}
+
+label {
+
+  display: inline-block;
+  margin-bottom: 10px;
+  margin-right: 20px;
+} */
 
 </style>

f6bb4fa355ea4df46e14ec5ac916bfefc76f2be0
Rich King <rking@rebgv.org>
2020-11-13 16:29:05 -0800

    Switched to PrimeVue InputText component

--- src/components/FormText.vue
+++ src/components/FormText.vue
@@ -1,6 +1,12 @@
 <template>
   
-  <input type="text" :name="name" :placeholder="friendlyName">
+  <div class="container p-float-label">
+
+    
+    <InputText :id="name" v-model="textTest" />
+    <label :for="name">{{ friendlyName }}</label> 
+    
+  </div>
 
 </template>
 
@@ -8,9 +14,19 @@
 
 <script>
 
+import InputText from 'primevue/inputtext';
+
 export default {
   
   name: 'FormText',
+  components: {
+    'InputText': InputText
+  },
+  data () {
+    return {
+      textTest: null
+    }
+  },
   props: [
     'friendlyName',
     'name'
@@ -23,6 +39,26 @@ export default {
 
 <style scoped>
 
+.container {
 
+  display: inline-block;
+  margin-right: 10px;
+  margin-bottom: 12px;
+}
+
+input {
+
+  display: block;
+  border-radius: 4px;
+}
+
+/* label {
+
+  display: block;
+  font-size: 11px;
+  margin-left: 1px;
+  margin-bottom: 5px;
+} */
 
 </style>
+

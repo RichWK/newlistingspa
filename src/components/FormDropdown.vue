5644c69a637d20a92f0f94a26784514a34214293
Rich King <rking@rebgv.org>
2020-11-13 19:10:03 -0800

    Added support for PrimeVue dropdown component

---
+++ src/components/FormDropdown.vue
@@ -0,0 +1,52 @@
+<template>
+  
+  <PrimeDropdown
+    v-model="testing"
+    :placeholder="friendlyName"
+    :options="dropdownOptions"
+    optionLabel="name"
+    filter
+  />
+
+</template>
+
+
+
+<script>
+
+import Dropdown from 'primevue/dropdown';
+
+export default {
+  
+  name: 'FormCheckbox',
+  components: {
+    'PrimeDropdown': Dropdown
+  },
+  data () {
+    return {
+      'testing': null
+    }
+  },
+  props: [
+    'dropdownOptions',
+    'friendlyName',
+    'name'
+  ]
+}
+
+</script>
+
+
+
+<style scoped>
+
+.p-dropdown {
+
+  min-width: 216px;
+  margin-right: 12px;
+  margin-bottom: 20px;
+}
+
+
+</style>
+

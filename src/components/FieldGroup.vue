d9934b840c1aac0821686259bc1987b0fdecd16a
Rich King <rking@rebgv.org>
2020-11-17 13:22:29 -0800

    Computed props rather than a combo of v-for & v-if

--- src/components/FieldGroup.vue
+++ src/components/FieldGroup.vue
@@ -4,16 +4,12 @@
 
   <h2>{{ name }}</h2>
 
-  <template v-for="field in fields" :key="field.name">
-
-      <FormText v-if="field.fType === 'text' || field.fType === 'phone'" v-bind="field"/>
-      <FormDropdown v-if="field.fType === 'dropdown'" v-bind="field"/>
-      <FormNumber v-if="field.fType === 'number'" v-bind="field"/>
-      <FormDate v-if="field.fType === 'date'" v-bind="field"/>
-      <FormCheckbox v-if="field.fType === 'checkbox'" v-bind="field"/>
-      <FormRadio v-if="field.fType === 'radio'" v-bind="field"/>
-
-  </template>
+  <FormText v-for="field in textFields" :key="field.name" v-bind="field" />
+  <FormDate v-for="field in dateFields" :key="field.name" v-bind="field"/>
+  <FormDropdown v-for="field in dropdownFields" :key="field.name" v-bind="field"/>
+  <FormCheckbox v-for="field in checkboxFields" :key="field.name" v-bind="field"/>
+  <FormRadio v-for="field in radioFields" :key="field.name" v-bind="field"/>
+  <!-- <FormNumber v-for="field in numberFields" :key="field.name" v-bind="field"/> -->
 
 </div>
 
@@ -24,22 +20,44 @@
 <script>
 
 import FormText from './FormText.vue'
-import FormDropdown from './FormDropdown.vue'
-import FormNumber from './FormNumber.vue'
 import FormDate from './FormDate.vue'
+import FormDropdown from './FormDropdown.vue'
 import FormCheckbox from './FormCheckbox.vue'
 import FormRadio from './FormRadio.vue'
+// import FormNumber from './FormNumber.vue'
 
 export default {
   
   name: 'FieldGroup',
+  computed: {
+    textFields() {
+      return this.fields.filter( f => this.textFieldTypes.includes( f.fType ) );
+    },
+    dateFields() {
+      return this.fields.filter( f => f.fType === 'date' );
+    },
+    dropdownFields() {
+      return this.fields.filter( f => f.fType === 'dropdown' );
+    },
+    checkboxFields() {
+      return this.fields.filter( f => f.fType === 'checkbox' );
+    },
+    radioFields() {
+      return this.fields.filter( f => f.fType === 'radio' );
+    }
+  },
   components: {
     'FormText': FormText,
-    'FormDropdown': FormDropdown,
-    'FormNumber': FormNumber,
     'FormDate': FormDate,
+    'FormDropdown': FormDropdown,
     'FormCheckbox': FormCheckbox,
     'FormRadio': FormRadio
+    // 'FormNumber': FormNumber
+  },
+  data() {
+    return {
+      textFieldTypes: ['text', 'phone']
+    }
   },
   props: {
     'name': String,

967197991eb4b22682edb96e7784c4732160ebd5
Rich King <rking@rebgv.org>
2020-12-15 17:45:34 -0800

    Hiding the dropdown icon once a value is selected

--- src/components/FormDropdown.vue
+++ src/components/FormDropdown.vue
@@ -69,11 +69,34 @@ export default {
 
 </style>
 
+
+
 <style>
+
+/* These are global. */
 
 .p-dropdown-panel div.p-dropdown-header {
 
   padding: 0;
+}
+
+/* The styles below hide the dropdown icon once a value has been selected. */
+
+i.p-dropdown-clear-icon {
+
+  top: 0;
+  margin-top: 0;
+}
+
+div.p-dropdown i.p-dropdown-clear-icon {
+
+  right: 0;
+  padding: 11px;
+}
+
+i.p-dropdown-clear-icon + div.p-dropdown-trigger {
+
+  display: none;
 }
 
 </style>
